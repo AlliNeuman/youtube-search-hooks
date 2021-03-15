8e17738eeaebcaefcd47e276f92a36bcab1f805e
Alli Neuman <alli.neuman@gmail.com>
2021-03-15 14:39:18 -0500

    add styling and updates for video detail and defaults for reloading and initial preload

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,10 +1,24 @@
+import "./VideoDetail.css";
 import React from "react";
 
 const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>Loading...</div>;
   } else {
-    return <div>{video.snippet.title}</div>;
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+    return (
+      <div>
+        <div className="ui segment video-detail">
+          <div className="ui embed">
+            <iframe src={videoSrc} title={video.snippet.title} />
+          </div>
+          <div className="content">
+            <h4 className="ui header">{video.snippet.title}</h4>
+            <p>{video.snippet.description}</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 };
 
